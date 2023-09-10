853e7ce45b0cc43178f2909988f7eb34154db42b
Avaneesh-Acharya <137638840+Avaneesh-Acharya@users.noreply.github.com>
2023-09-10 09:19:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
+var S=""
 function preload() {
-    img1 = loadImage("bedroom.jpg")
-    img2 = loadImage("tvac.jpg")
-    img3 = loadImage("image84734.jpg")
-    img4 = loadImage("coke.jpg")
+    img1 = loadImage("bedroom.jpeg")
+    img2 = loadImage("tvac.jpeg")
+    img3 = loadImage("image84734.jpeg")
+    img4 = loadImage("coke.jpeg")
     img5 = loadImage("FYF-870.webp")
 }
 function setup() {
     canvas = createCanvas(620.893, 423.456)
     canvas.center()
 }
-function draw() 
-    s = document.getElementById("id").value
-    if (s == "bedroom") {
+function draw() {
+   var S = document.getElementById("bedroom").textContent
+    console.log(S)
+    if (S == "bedroom") {
         img = img1
     }
-    if (s == "TV_AC") {
+    if (S == "TV_AC") {
         img = img2
 
-    } if (s == "desk") {
+    } if (S == "desk") {
         img = img3
 
-    } if (s == "bottles") {
+    } if (S == "bottles") {
         img = img4
 
-    } if (s == "fruitbasket") {
+    } if (S == "fruitbasket") {
         img = img5
 
-
+    }
     image(img, 0, 0, 620.893, 423.456)
     fill("#4169e1")
     text("Dog", 45, 75)
